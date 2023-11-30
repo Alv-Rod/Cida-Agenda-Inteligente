ea6d03ba47088502b5efbb6da02603009dd5e3f8
Álvaro Rodrigues M. de Menezes <alvaro.rodrmen@gmail.com>
2023-11-30 18:31:43 -0300

    Add files via upload

---
+++ agenda_create_prompts.sql
@@ -0,0 +1,21 @@
+/*
+CREATE DATABASE IF NOT EXISTS agenda;
+
+CREATE TABLE IF NOT EXISTS events_table (
+	eventId INT PRIMARY KEY AUTO_INCREMENT,
+    eventName VARCHAR(60) NOT NULL,
+    eventDay DATE NOT NULL,
+    eventDescription VARCHAR(255),
+    eventStart TIME,
+    eventEnd TIME,
+    eventIsAllDay BOOL,
+    eventType VARCHAR(60)
+)*/
+
+
+
+CREATE TABLE IF NOT EXISTS user_table (
+	userId INT PRIMARY KEY AUTO_INCREMENT,
+    userName VARCHAR(60) NOT NULL,
+    userEmail VARCHAR(255) NOT NULL
+)
